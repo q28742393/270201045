f1fd0a1375174119e0019af502830342d1e14c69
q28742393 <altair00101010@gmail.com>
2020-12-28 09:27:55 +0000

    .

--- lab1/example1.py
+++ lab1/example1.py
@@ -1,13 +1,11 @@
-def hailstone(n):
-  s=str(n)
+def sum_of_a_nested_list(x):
+   if not isinstance(x, list):
+     return x
+   else:
+     sum = 0
+     for a in x:
+        sum += sum_of_a_nested_list(a)
+     return sum
 
-  if n==1:
-    return s
 
-  if n%2 ==0:
-    return s+","+hailstone(n//2)
-  else:
-    return s+","+hailstone((3*n)+1)
-
-print(hailstone(11))
-  
+print(sum_of_a_nested_list([3, 12, 76, [4, 56, 43], [2, 8], 81, 75]))
