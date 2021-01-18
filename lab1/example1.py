4d1f43cba44736b33ac83a7266fa567276662633
q28742393 <altair00101010@gmail.com>
2021-01-18 09:00:49 +0000

    .

--- lab1/example1.py
+++ lab1/example1.py
@@ -1,26 +1,15 @@
-import math
-class Cylinder:
- def __init__(self,radius,height):
-  self.set_radius(radius)
-  self.set_height(height)
- def get_radius(self):
-  return self.radius
- def set_radius(self,radius):
-  if radius>0:
-    self.radius = radius
- def get_height(self):
-   return self.height
- def set_height(self,height):
-  if height>0:
-   self.height = height
- def calc_base_area(self):
-   return math.pi * (self.radius**2)
- def calc_surface_area(self):
-   return 2 * (math.pi * self.radius) * self.height
- def calc_area(self):
-  return 2 * self.calc_base_area() + self.calc_surface_area()
- def calc_volume(self):
-  return self.calc_base_area() * self.height
-cylinder = Cylinder(3,5)
-print ("Area:",cylinder.calc_area())
-print ("Volume:",cylinder.calc_volume())
+def selSort(nums):
+
+  n =len(nums)
+  for bottom in range(n-1):
+
+    mp=bottom
+    for i in range(bottom+1,n):
+      if nums[i]<nums [mp]:
+        mp=i
+
+    nums[bottom],nums[mp]=nums[mp],nums[bottom]    
+
+sort_list=[55,25,78,2,17,98,13,5]
+
+selSort(sort_list)
